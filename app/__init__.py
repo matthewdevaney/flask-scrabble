2f2b3574a1a18f67705d8dd7a08924bdea57ed43
mdevaney <mbdevaney@gmail.com>
2019-08-03 21:09:49 -0500

    add sqllite to project

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+form config import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 Bootstrap(app)
 
-from app import routes
+from app import models, routes
 
 if __name__ == '__main__':
     app.run(debug=True)
