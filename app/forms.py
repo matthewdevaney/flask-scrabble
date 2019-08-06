c8824851be2af715a07f95de9ee40de39ae13d29
Matthew Devaney <mdevaney@fwsgroup.com>
2019-08-06 11:53:34 -0500

    bootstrap theme added

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Regexp
 
 class SearchForm(FlaskForm):
-    letters = StringField('Your Letters', validators=[DataRequired()])
+    letters = StringField('Your Letters', validators=[Length(min=2, max=15, message='must be between 2-15 letters'),
+                                                      Regexp('[a-zA-Z]+', message='must only contain letters')])
     submit = SubmitField('Submit')
